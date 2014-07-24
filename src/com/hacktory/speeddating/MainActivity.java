7576322d20e076a58c80e0a8f751e0270a2a2e25
TomyRO <alex.dan.tomescu@gmail.com>
2014-07-24 19:29:15 +0200

    initial commit

---
+++ src/com/hacktory/speeddating/MainActivity.java
@@ -0,0 +1,33 @@
+package com.hacktory.speeddating;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//startButton = (Button) findViewById(R.id.start_service_button);
+		//Log.d("Activity", startButton.toString());
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+}
